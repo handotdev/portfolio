6eceaa98bb20dba79b755491f6a930f8100eb7b3
Han Wang <hanwang@dhcp-gs-6530.redrover.cornell.edu>
2019-03-15 17:59:38 -0400

    Change timeout to 1500 milliseconds

--- js/redirect.js
+++ js/redirect.js
@@ -1,3 +1,3 @@
 function openInNewTab(url) {
-  setTimeout(function(){var win = window.open(url, '_blank'); win.focus()}, 3000);
+  setTimeout(function(){var win = window.open(url, '_blank'); win.focus()}, 1500);
 }
