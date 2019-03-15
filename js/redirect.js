39adf0ff009a37e2f40457ef696d8f49612977c5
Han Wang <hanwang@dhcp-gs-6530.redrover.cornell.edu>
2019-03-15 17:58:40 -0400

    Add focus

--- js/redirect.js
+++ js/redirect.js
@@ -1,3 +1,3 @@
 function openInNewTab(url) {
-  setTimeout(function(){var win = window.open(url, '_blank')}, 5000);
+  setTimeout(function(){var win = window.open(url, '_blank'); win.focus()}, 3000);
 }
